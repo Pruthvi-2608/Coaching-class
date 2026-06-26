43d14e5bbba84ee5f40bf9592c895de98cf715d8
Pruthvi-2608 <pruthvibpanchal10@gmail.com>
2026-06-26 12:42:36 +0530

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'force-exit-after-build',
+      apply: 'build',
+      closeBundle() {
+        process.exit(0);
+      }
+    }
+  ],
   server: {
     port: 3000,
     open: true
